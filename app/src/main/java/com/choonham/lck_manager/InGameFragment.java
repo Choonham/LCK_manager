7c00e60c22cd22c380624267e0f7b600269af19c
choonham <dog026@naver.com>
2023-01-31 08:28:45 +0900

    [2023.01.31 노영준] In-Game Fragment에 tab layout 추가 및 맵 이미지 추가

--- app/src/main/java/com/choonham/lck_manager/InGameFragment.java
+++ app/src/main/java/com/choonham/lck_manager/InGameFragment.java
@@ -1,10 +1,15 @@
 package com.choonham.lck_manager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -13,6 +18,7 @@ import android.view.ViewGroup;
  */
 public class InGameFragment extends Fragment {
 
+    TabLayout tabLayout;
     public InGameFragment() {
         // Required empty public constructor
     }
@@ -33,7 +39,72 @@ public class InGameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_in_game, container, false);
+
+        ImageView map = rootView.findViewById(R.id.in_game_lift_map_image_view);
+
+        ViewTreeObserver vto = map.getViewTreeObserver();
+
+        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                map.getViewTreeObserver().removeOnPreDrawListener(this);
+
+                MapFogView fogView = new MapFogView(getContext(), 1, map.getMeasuredWidth(), map.getMeasuredHeight());
+
+                FrameLayout fogLayout = rootView.findViewById(R.id.in_game_fog_layout);
+                fogLayout.addView(fogView);
+
+                Log.d("돌아", "돌아");
+
+                return true;
+            }
+        });
+
+
+        InGameHpFragment hpFragment = new InGameHpFragment();
+        InGameActionFragment actionFragment = new InGameActionFragment();
+        InGameStatusFragment statusFragment = new InGameStatusFragment();
+        InGameGoldFragment goldFragment = new InGameGoldFragment();
+
+        getParentFragmentManager().beginTransaction().replace(R.id.in_game_frame, goldFragment).commit();
+
+        tabLayout = rootView.findViewById(R.id.in_game_tabLayout);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+
+                if(position == 0) {
+                    selected = goldFragment;
+                } else if(position == 1) {
+                    selected = hpFragment;
+                } else if(position == 2) {
+                    selected = statusFragment;
+                } else if(position == 3) {
+                    selected = actionFragment;
+                } else {
+                    selected = goldFragment;
+                }
+
+                getParentFragmentManager().beginTransaction().replace(R.id.in_game_frame, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_in_game, container, false);
+        return rootView;
     }
 }
